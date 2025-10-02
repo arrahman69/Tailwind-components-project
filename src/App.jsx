d7977aa877cef9ba4ec5f7238e5af92e421f0a38
Abdur Rahman <tohaali332@gmail.com>
2025-10-03 01:46:34 +0600

    navbar work finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import DaisyNav from './components/DaisyNav/DaisyNav';
 import NavBar from './components/NavBar/NavBar';
+import Spinner from './components/Spiner/Spinner';
 
 const App = () => {
   return (
     <div>
     <NavBar></NavBar>
-<DaisyNav></DaisyNav>
+    <Spinner></Spinner>
+{/* <DaisyNav></DaisyNav> */}
   
     </div>
   );
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,55 +1,80 @@
-import React from 'react';
+import { Menu, X } from "lucide-react";
+import React, { useState } from "react";
 
 const navigationData = [
   {
     id: 1,
     name: "Dashboard",
-    path: "/dashboard"
+    path: "/dashboard",
   },
   {
     id: 2,
     name: "Projects",
-    path: "/projects"
+    path: "/projects",
   },
   {
     id: 3,
     name: "Teams",
-    path: "/teams"
+    path: "/teams",
   },
   {
     id: 4,
     name: "Settings",
-    path: "/settings"
+    path: "/settings",
   },
   {
     id: 5,
     name: "Profile",
-    path: "/profile"
-  }
+    path: "/profile",
+  },
 ];
 
-
 const NavBar = () => {
-    return (
-        <nav>
+  const [open, setOpen] = useState(false);
+  const links = navigationData.map((route) => (
+    <link key={route.id} route={route}></link>
+  ));
 
-<li className='flex justify-end'>
-    { navigationData.map(route=><link key={route.id}route= {route}></link>)}
-</li>
+  return (
+    <div className="mx-10">
+      <nav className="flex justify-between mx-5 mt-4">
+        <span className="flex" onClick={() => setOpen(!open)}>
+          {open ? (
+            <X className="md:hidden"></X>
+          ) : (
+            <Menu className="md:hidden"></Menu>
+          )}
 
-            <ul className='flex justify-end'>
-{
-    navigationData.map(route => <li className='mr-10'>
-        <a href={route.path}>{route.name}</a></li> )
-}
-            </ul>
-            {/* <ul className='flex justify-end'>
+          {/* <ul className="">{links}</ul> */}
+          <h3 className="ml-4">My Navbar</h3>
+        </span>
+
+        <ul className="flex md:hidden">{links}</ul>
+
+        <ul className="md:flex hidden">
+          {navigationData.map((route) => (
+            <li className="mr-10 ">
+              <a href={route.path}>{route.name}</a>
+            </li>
+          ))}
+        </ul>
+        {/* <ul className='flex'>
                 <li className='mr-10'><a href="/">Home</a></li>
              <li className='mr-10'><a href="/about">About</a></li>
                 <li className='mr-10'><a href="/blog">Blog</a></li>
             </ul> */}
-        </nav>
-    );
+
+        <button>sign In</button>
+      </nav>
+      <ul className={` ${open ? "flex flex-col top-10 left-22" : "-top-50 left-22"}  absolute duration-1000 transition-all bg-gray-500 -top-50`}>
+        {navigationData.map((route) => (
+          <li className="lg:mr-18 hover:bg-gray-600 m-1 p-0.4">
+            <a href={route.path}>{route.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default NavBar;
